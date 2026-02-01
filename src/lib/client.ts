d1f5e84056ffe05b68a842da97ac959dcf996a88
Shubhankar <shubhankar.sharma97@gmail.com>
2026-02-01 22:48:25 +0530

    .

--- src/lib/client.ts
+++ src/lib/client.ts
@@ -3,6 +3,7 @@ import type { App } from '../app/api/[[...slugs]]/route'
 
 
 // .api to enter /api prefix
-export const client = treaty<App>("localhost:3000").api
+export const client = treaty<App>(
+    process.env.NEXT_PUBLIC_API_URL ??"").api
     
 
