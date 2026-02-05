2620002583b30be0c7aba313694ee6fd87477a31
Shubhankar <shubhankar.sharma97@gmail.com>
2026-02-05 19:34:41 +0530

    ..

--- src/lib/client.ts
+++ src/lib/client.ts
@@ -1,8 +1,9 @@
 import { treaty } from '@elysiajs/eden'
 import type { App } from '../app/api/[[...slugs]]/route'
 
+const apiBase =
+  process.env.NEXT_PUBLIC_BASE_URL ??
+  (typeof window !== 'undefined' ? window.location.origin : 'http://localhost:3000')
 
 // .api to enter /api prefix
-export const client = treaty<App>(process.env.NODE_ENV === "development" ? "http://localhost:3000" : "https://treaty.vercel.app").api
-    
-
+export const client = treaty<App>(apiBase).api
